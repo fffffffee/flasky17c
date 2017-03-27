d31db37c98303642774fed9ecb125acc541934cc
fffffff7 <light_black@126.com>
2017-03-27 22:47:24 +0800

    modified mailinfo to google

--- info.py
+++ info.py
@@ -1,3 +1,6 @@
-MAILUSERNAME = 'light_black@126.com'
-MAILPASSWORD = 'fffffff77'	
+#MAILUSERNAME = 'light_black@126.com'
+#MAILPASSWORD = 'fffffff77'	
+#FLASKYADMIN  = 'light_black@126.com'
+MAILUSERNAME = 'ffffffflv@gmail.com'
+MAILPASSWORD = 'fffffff'
 FLASKYADMIN  = 'light_black@126.com'
